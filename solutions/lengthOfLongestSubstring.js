55e6208c947be7e5a75824990db3296f2e8b65a1
Young <199681@sh.lianjia.com>
2017-02-04 16:13:09 +0800

    finish longest substring without repeating characters

--- solutions/lengthOfLongestSubstring.js
+++ solutions/lengthOfLongestSubstring.js
@@ -2,7 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    if(!s || s.length === 1) return s;
-    var i,j
-};
+ /**
+  * @param {string} s
+  * @return {number}
+  */
+ var lengthOfLongestSubstring = function(s) {
+     if (s.length < 2) {
+         return s.length;
+     }
+     var max = 0;
+     var start = -1;
+     var map = {};
+     for (var i = 0; i < s.length; i++) {
+         var t = map[s[i]];
+         if (t !== undefined) {
+             start = Math.max(start,t);
+         }
+         map[s[i]] = i;
+         max = Math.max(max, i - start)
+     }
+     return max
+ };
