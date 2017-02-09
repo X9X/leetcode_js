a469bcc4a0a960e2f16c48b9394c62f32314a2a5
Young <199681@sh.lianjia.com>
2017-02-09 16:03:02 +0800

    add sortList.js and .babelrc

--- solutions/sortList.js
+++ solutions/sortList.js
@@ -10,5 +10,34 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-
+    function merge(a,b){
+        let head = new ListNode(null);
+        while(a && b){
+            if (a.val >= b.val){
+                head.next = b;
+                b = b.next;
+            } else {
+                head.next = a;
+                a = a.next
+            }
+        }
+        if(!a){
+            head.next = a
+        }
+        if(!b){
+            head.next = b
+        }
+        return head.next
+    }
+    let pre = null;
+    let low = head;
+    let fast = head;
+    // step1 cut linked list to 2 pieces
+    while(fast && fast.next){
+        pre = low;
+        low = low.next;
+        fast = fast.next.next
+    }
+    pre.next = null
+    return merge(sortList(head),sortList(low))
 };
